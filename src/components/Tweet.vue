57959cee5b2f3a44fc6d63ec5a82cac3f9ba7c63
Rupert Dunk <rupert@rupertdunk.com>
2017-06-20 16:59:36 +0100

    Simplify Tweet component

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -25,18 +25,13 @@ export default {
       required: true,
     },
   },
-  computed: {
-    id() {
-      return this.$t(this.content);
-    },
-  },
   mounted() {
     const scriptLoaded = !window.twttr
       ? addScript('//platform.twitter.com/widgets.js')
       : Promise.resolve();
 
     scriptLoaded.then(() => {
-      window.twttr.widgets.createTweetEmbed(this.id, this.$el, this.options);
+      window.twttr.widgets.createTweetEmbed(this.$t(this.content), this.$el, this.options);
       this.isTweetLoaded = true;
     });
   },
