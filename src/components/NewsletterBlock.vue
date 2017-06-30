58f4fef6331f7fe8c258b97e1832f62adc8fbfd7
Rupert Dunk <rupert@rupertdunk.com>
2017-06-30 09:44:39 +0100

    Add async submit functionality to newsletter form, remove unused inputs

--- src/components/NewsletterBlock.vue
+++ src/components/NewsletterBlock.vue
@@ -1,9 +1,7 @@
 <template>
   <form class="newsletter-block input-wrap" @submit.prevent="submit" enctype="multipart/form-data">
-
     <input type="hidden" name="CID" value="274418198">
     <input type="hidden" name="SID" value="">
-    <input type="hidden" name="UID" value="">
     <input type="hidden" name="f" value="8604">
     <input type="hidden" name="p" value="2">
     <input type="hidden" name="a" value="r">
@@ -12,8 +10,6 @@
     <input type="hidden" name="llid" value="">
     <input type="hidden" name="c" value="">
     <input type="hidden" name="counted" value="">
-    <input type="hidden" name="RID" value="">
-    <input type="hidden" name="mailnow" value="">
     <input type="hidden" name="inp_30577" size="1" value="1">
     <input type="hidden" name="inp_14051" :value="lang">
 
@@ -25,6 +21,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import { validateEmail } from '../util/validation';
 
 export default {
@@ -46,7 +43,7 @@ export default {
   },
   computed: {
     t() {
-      return this.$store.state.site.translations;
+      return this.$store.state.site.translations.newsletter;
     },
     lang() {
       return this.$store.state.lang === 'tc' ? 2 : 1;
@@ -61,17 +58,19 @@ export default {
       // @TODO Make sure this works
       /* eslint-disable no-undef */
       const formData = new FormData(this.$el);
-      formData.forEach((key, value) => console.log(value, key));
-      this.success();
-      return true;
+      return axios.post('http://stories.dev/', formData).then(() => {
+        this.success();
+        return true;
+      }).catch(() => {
+        this.error('There was a network problem. Please try again later');
+        return false;
+      });
     },
-    error() {
-      this.$modal.show(this.$t(this.t.newsletter.errorInvalid),
-      this.$t(this.t.newsletter.errorTitle));
+    error(message) {
+      this.$modal.show(message || this.$t(this.t.errorInvalid), this.$t(this.t.errorTitle));
     },
     success() {
-      this.$modal.show(this.$t(this.t.newsletter.successMessage),
-      this.$t(this.t.newsletter.successTitle));
+      this.$modal.show(this.$t(this.t.successMessage), this.$t(this.t.successTitle));
     },
   },
 };
