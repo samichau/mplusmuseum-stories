3428b3cb266b672607bd4402509f6642b2298a61
Rupert Dunk <rupert@rupertdunk.com>
2017-07-04 12:06:14 +0100

    Add support for fixed banners in basic page view

--- src/views/BasicView.vue
+++ src/views/BasicView.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="page--basic">
+  <div class="page--basic" :class="{'page--fix-banner': page.banner && page.banner.fixed }">
     <banner v-if="page.banner" :data="page.banner"></banner>
     <div v-else class="header-push"></div>
-    <content-blocks class="page__content"
-    v-if="page.sections"
-    :items="page.sections.list"></content-blocks>
+    <div class="page__wrap">
+      <content-blocks class="page__content"
+      v-if="page.sections"
+      :items="page.sections.list"></content-blocks>
+    </div>
   </div>
 </template>
 
@@ -26,25 +28,57 @@ export default {
 </script>
 
 <style lang="less">
-  .page--basic {
-    .page {
-      &__content {
-        margin: 5rem auto;
-        padding-left: 1.5rem;
-        padding-right: 1.5rem;
-        max-width: 1040px;
-        .block {
-          max-width: 800px;
-          margin-left: auto;
-          margin-right: auto;
-          &.image-block, &.youtube-block {
-            max-width: 100%;
-          }
-          &.instagram-block {
-            max-width: 600px;
-          }
+@import "../less/variables.less";
+
+.page {
+  &__wrap {
+    background: @white;
+    &:before, &:after{
+      content: ' ';
+      display: table;
+    }
+  }
+}
+.page--basic {
+  .page {
+    &__content {
+      margin: 2em auto;
+      padding-left: 1.5rem;
+      padding-right: 1.5rem;
+      // padding: 0 1.5rem;
+      max-width: 1040px;
+      .block {
+        max-width: 800px;
+        margin-left: auto;
+        margin-right: auto;
+        &.image-block, &.youtube-block {
+          max-width: 100%;
+        }
+        &.instagram-block {
+          max-width: 600px;
         }
       }
     }
   }
+}
+.page--fix-banner {
+  padding-top: 100%;
+  position: relative;
+  .mq-sm({
+    padding-top: 80%;
+  });
+  .mq-md({
+    padding-top: 60%;
+  });
+  .mq-lg({
+    padding-top: 40%;
+  });
+  .banner {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: -1;
+  }
+}
 </style>
