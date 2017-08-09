d5e9f84df9bec5bdc9c9622625fb2ab24e423c3f
Rupert Dunk <rupert@rupertdunk.com>
2017-08-09 13:51:22 +0100

    Fix unreactive sticky properies

--- src/components/Sticky.vue
+++ src/components/Sticky.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="sticky"
   :class="{ 'sticky--stuck': stuck }"
-  :style="styles">
-    <div class="sticky__content" :style="contentStyles">
+  :style="{
+    height,
+    opacity,
+    visibility,
+  }">
+    <div class="sticky__content" :style="{
+      top,
+      width,
+    }">
       <slot></slot>
     </div>
   </div>
@@ -20,19 +27,13 @@ export default {
     return {
       stuck: false,
       overflows: false,
-      measurements: {
-        height: 0,
-        windowHeight: 0,
-      },
-      styles: {
-        height: 'inherit',
-        opacity: 'inherit',
-        visibility: 'inherit',
-      },
-      contentStyles: {
-        top: 'inherit',
-        width: 'inherit',
-      },
+      contentHeight: 0,
+      windowHeight: 0,
+      height: 'inherit',
+      opacity: 'inherit',
+      visibility: 'inherit',
+      top: 'inherit',
+      width: 'inherit',
     };
   },
   mounted() {
@@ -54,7 +55,7 @@ export default {
     },
     shouldBeStuck() {
       const pos = this.$el.getBoundingClientRect().top;
-      if ((pos + this.measurements.height) <= this.measurements.windowHeight) return pos;
+      if ((pos + this.contentHeight) <= this.windowHeight) return pos;
       return false;
     },
     setHeight(offset = false) {
@@ -62,22 +63,22 @@ export default {
       const h = content.offsetHeight
       + parseInt(window.getComputedStyle(content).marginTop, 10)
       + parseInt(window.getComputedStyle(content).marginBottom, 10);
-      this.styles.height = `${h}px`;
-      if ((this.measurements.height > this.measurements.windowHeight) && offset) {
-        this.contentStyles.top = `${offset}px`;
+      this.height = `${h}px`;
+      if ((this.contentHeight > this.windowHeight) && offset) {
+        this.top = `${offset}px`;
       } else {
-        this.contentStyles.top = 'inherit';
+        this.top = 'inherit';
       }
     },
     setWidth() {
       const w = this.$el.offsetWidth;
-      this.contentStyles.width = `${w}px`;
+      this.width = `${w}px`;
     },
     getMeasurements() {
-      this.measurements.height = this.$el.offsetHeight;
-      this.measurements.windowHeight = Math.max(
+      this.contentHeight = this.$el.offsetHeight;
+      this.windowHeight = Math.max(
         document.documentElement.clientHeight, window.innerHeight || 0);
-      const overflows = this.measurements.height > this.measurements.windowHeight;
+      const overflows = this.contentHeight > this.windowHeight;
       this.overflows = overflows;
       return overflows;
     },
@@ -87,9 +88,9 @@ export default {
       this.stuck = true;
     },
     unstick() {
-      this.styles.height = 'inherit';
-      this.contentStyles.top = 'inherit';
-      this.contentStyles.width = 'inherit';
+      this.height = 'inherit';
+      this.top = 'inherit';
+      this.width = 'inherit';
       this.stuck = false;
     },
     resize() {
@@ -116,8 +117,8 @@ export default {
       const pos = window.scrollY || window.pageYOffset;
       const opacity = Math.max(1 - (pos / 350), 0);
       const visibility = opacity === 0 ? 'hidden' : 'visible';
-      this.styles.opacity = opacity;
-      this.styles.visibility = visibility;
+      this.opacity = opacity;
+      this.visibility = visibility;
     },
   },
 };
