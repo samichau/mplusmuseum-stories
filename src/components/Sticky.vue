c26243d23226ff5ccd990762e17f77ae1aef3cdd
Rupert Dunk <rupert@rupertdunk.com>
2017-08-09 12:31:30 +0100

    Rewrite sticky component and add support for overflowing elements

--- src/components/Sticky.vue
+++ src/components/Sticky.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="sticky"
   :class="{ 'sticky--stuck': stuck }"
-  :style="styleObj">
-    <div class="sticky__content" ref="content">
+  :style="styles">
+    <div class="sticky__content" :style="contentStyles">
       <slot></slot>
     </div>
-    <div class="stick__spacer" ref="spacer"></div>
   </div>
 </template>
 
@@ -20,57 +19,113 @@ export default {
   data() {
     return {
       stuck: false,
-      styleObj: {},
+      overflows: false,
+      measurements: {
+        height: 0,
+        windowHeight: 0,
+      },
+      styles: {
+        height: 'inherit',
+        opacity: 'inherit',
+        visibility: 'inherit',
+      },
+      contentStyles: {
+        top: 'inherit',
+        width: 'inherit',
+      },
     };
   },
   mounted() {
-    this.stick();
+    this.init();
     // Add load event listener as fix for iOS incorrectly calculating element height
-    window.addEventListener('load', this.stick, { once: true });
+    window.addEventListener('load', this.init, { once: true });
     window.addEventListener('resize', this.resize);
-    if (this.fadeOut) window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('scroll', this.onScroll);
   },
   destroyed() {
-    window.removeEventListener('load', this.stick);
+    window.removeEventListener('load', this.init);
     window.removeEventListener('resize', this.resize);
-    if (this.fadeOut) window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('scroll', this.onScroll);
   },
   methods: {
-    setHeight() {
+    init() {
+      const elementOverflows = this.getMeasurements();
+      if (!elementOverflows && !this.stuck) this.stick();
+    },
+    shouldBeStuck() {
+      const pos = this.$el.getBoundingClientRect().top;
+      if ((pos + this.measurements.height) <= this.measurements.windowHeight) return pos;
+      return false;
+    },
+    setHeight(offset = false) {
       const content = this.$slots.default[0].elm; // Only allows for single parent element in slot
       const h = content.offsetHeight
       + parseInt(window.getComputedStyle(content).marginTop, 10)
       + parseInt(window.getComputedStyle(content).marginBottom, 10);
-      this.$refs.spacer.style.height = `${h}px`;
+      this.styles.height = `${h}px`;
+      if ((this.measurements.height > this.measurements.windowHeight) && offset) {
+        this.contentStyles.top = `${offset}px`;
+      } else {
+        this.contentStyles.top = 'inherit';
+      }
     },
     setWidth() {
       const w = this.$el.offsetWidth;
-      this.$refs.content.style.width = `${w}px`;
+      this.contentStyles.width = `${w}px`;
     },
-    stick() {
+    getMeasurements() {
+      this.measurements.height = this.$el.offsetHeight;
+      this.measurements.windowHeight = Math.max(
+        document.documentElement.clientHeight, window.innerHeight || 0);
+      const overflows = this.measurements.height > this.measurements.windowHeight;
+      this.overflows = overflows;
+      return overflows;
+    },
+    stick(offset = false) {
       this.setWidth();
-      this.setHeight();
+      this.setHeight(offset);
       this.stuck = true;
     },
-    resize() {
-      this.stick();
-      // @TODO Implement more reliable strategy for waiting until DOM has updated
-      setTimeout(this.setHeight, 250);
+    unstick() {
+      this.styles.height = 'inherit';
+      this.contentStyles.top = 'inherit';
+      this.contentStyles.width = 'inherit';
+      this.stuck = false;
     },
-    handleScroll() {
+    resize() {
+      const elementOverflows = this.getMeasurements();
+      const shouldBeStuck = this.shouldBeStuck();
+      if (shouldBeStuck !== false && !elementOverflows) {
+        this.stick();
+      } else if (shouldBeStuck === false && this.stuck) {
+        this.unstick();
+      }
+    },
+    onScroll() {
+      if (this.overflows) {
+        const shouldBeStuck = this.shouldBeStuck();
+        if (shouldBeStuck !== false && !this.stuck) {
+          this.stick(shouldBeStuck);
+        } else if (shouldBeStuck === false && this.stuck) {
+          this.unstick();
+        }
+      }
+      if (this.fadeOut) this.handleFade();
+    },
+    handleFade() {
       const pos = window.scrollY || window.pageYOffset;
       const opacity = Math.max(1 - (pos / 350), 0);
       const visibility = opacity === 0 ? 'hidden' : 'visible';
-      this.styleObj = {
-        opacity,
-        visibility,
-      };
+      this.styles.opacity = opacity;
+      this.styles.visibility = visibility;
     },
   },
 };
 </script>
 
 <style lang="less">
+@import "../less/variables.less";
+
 .sticky {
   position: relative;
   &--stuck {
